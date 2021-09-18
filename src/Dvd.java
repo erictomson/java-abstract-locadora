44d1562649a89bfc4e28c196504a9ebed30816ed
erictomson <erictomson@gmail.com>
2021-09-18 12:35:48 -0300

    Implementada classe DVD

--- src/Dvd.java
+++ src/Dvd.java
@@ -1,23 +1,65 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Dvd extends ItemAbstrato {
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     // Construtor da classe
     public Dvd() {}
 
-    // Método específico da classe DVD
-    public void vender() {}
 
     // Implementação (sobreescrita) de métodos
     // definidos na superclasse ItemAbstrato
     // @Override: anotação para indicar uma sobreescrita de método
     // definido na superclasse (e não um novo método na subclasse atual)
     @Override
-    public void emprestar() {}
+    public void cadastrar() {
+        setCodigo(1);
+        setTitulo("Senhor dos Anéis");
+        setSituacaoItem("L");
+        System.out.println("DVD Cadastrado: " +
+                        " - Código: " + getCodigo() +
+                        " - Título: " + getTitulo() +
+                        " - Situação: " + getSituacaoItem());
+        System.out.println("DVD cadastrado utilizando método abstrato " +
+                            "herdado da classe abstrata ItemAbstrato");
+    }
 
     @Override
-    public void cadastrar() {}
+    public void emprestar() {
+        setSituacaoItem("E");
+        setDataEmprestimo(sdf.format(new Date()));
+        System.out.println("DVD emprestado: " +
+                        " - Situação: " + getSituacaoItem() +
+                        " - Data Empréstimo: " + this.getDataEmprestimo());
+        System.out.println("DVD emprestrado utilizando método abstrato " +
+                "herdado da classe abstrata ItemAbstrato");
+    }
 
     @Override
-    public void devolver() {}
+    public void devolver() {
+        setSituacaoItem("E");
+        setDataDevolucao(sdf.format(new Date()));
+        System.out.println("DVD devolvido: " +
+                " - Situação: " + getSituacaoItem() +
+                " - Data Devolução: " + this.getDataEmprestimo());
+        System.out.println("DVD cadastrado utilizando método abstrato " +
+                "herdado da classe abstrata ItemAbstrato");
+    }
 
-    //public Dvd getInstance() {}
+    // Métodos específicos da classe DVD
+    // Imprimir lista de DVDs
+    public void imprimir() {
+        System.out.println("Imprimir Lista de DVDs utilizando método" +
+                "concreto da classe DVD");
+    }
+
+    // Retorna uma instância da classe DVD para o main
+    public static Dvd getInstance() {
+        return new Dvd();
+    }
+
+    // Método definido no UML, mas não implementado
+    public void vender() {}
+
 }
--- src/ItemAbstrato.java
+++ src/ItemAbstrato.java
@@ -6,10 +6,52 @@ public abstract class ItemAbstrato {
     private String dataDevolucao;
     private String situacaoItem; //L: Livre | E: Emprestado
 
+    // Getters e Setters dos atributos da classe
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(String dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public String getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(String dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public String getSituacaoItem() {
+        return situacaoItem;
+    }
+
+    public void setSituacaoItem(String situacaoItem) {
+        this.situacaoItem = situacaoItem;
+    }
+
     // Métodos abstratos a serem implementados nas subclasses
     public abstract void cadastrar();
 
     public abstract void emprestar();
 
     public abstract void devolver();
+
 }
